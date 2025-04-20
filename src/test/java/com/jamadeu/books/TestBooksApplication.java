8dddae617379bc09e3bc79e093d5731343fa30f9
Jean Amadeu <jean.amadeu2@gmail.com>
2025-04-20 09:31:29 -0300

    start project

---
+++ src/test/java/com/jamadeu/books/TestBooksApplication.java
@@ -0,0 +1,11 @@
+package com.jamadeu.books;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(BooksApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
